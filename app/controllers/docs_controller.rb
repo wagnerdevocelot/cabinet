e6b54c29f239e341f977b8b2fd83a81da4872ad8
vapordev <wagner.dev1337@gmail.com>
2021-01-05 00:35:03 -0300

    done

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,43 @@
+class DocsController < ApplicationController
+    
+    def index
+    
+    end
+    
+    def show
+    
+    end
+
+    def new
+    
+    end
+
+    def create
+    
+    end
+
+    def edit
+    
+    end
+
+    def update
+    
+    end
+
+    def destroy
+    
+    end
+
+    private
+
+    def doc_params
+
+    end
+
+    def doc_set
+
+    end
+
+
+
+end
