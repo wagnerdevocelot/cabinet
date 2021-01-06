a32f5533bcaf3568517b63e136145938672d2fbb
vapordev <wagner.dev1337@gmail.com>
2021-01-05 21:35:55 -0300

    doc forms new

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -9,11 +9,18 @@ class DocsController < ApplicationController
     end
 
     def new
-    
+        @doc = Doc.new
     end
 
     def create
-    
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render :new
+        end
+
     end
 
     def edit
@@ -31,10 +38,10 @@ class DocsController < ApplicationController
     private
 
     def doc_params
-
+        params.require(:doc).permit(:tite, :content)
     end
 
-    def doc_set
+    def find_doc
 
     end
 
